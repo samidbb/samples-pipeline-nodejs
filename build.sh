280ff98ed6d435a83191d8b3564c941e577d9154
Thomas Fischer <thfis@dfds.com>
2018-06-20 14:39:27 +0200

    initial build.sh

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+for manifest in ./k8s/*.yml; do
+    # echo "##vso[artifact.upload containerfolder=testresult;artifactname=uploadedresult;]c:\testresult.trx"
+    echo "##vso[artifact.associate type=filepath;artifactname=manifests]${manifest}"
+done
+
