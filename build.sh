d90739455cd4b245d975caabd9e55434c02d4245
Thomas Fischer <thfis@dfds.com>
2018-06-20 14:59:08 +0200

    use upload for artifacts

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -eu -o pipefail
 
 for manifest in ./k8s/*.yml; do
-    # echo "##vso[artifact.upload containerfolder=testresult;artifactname=uploadedresult;]c:\testresult.trx"
-    echo "##vso[artifact.associate type=container;artifactname=manifests]#${manifest}"
+    echo "##vso[artifact.upload containerfolder=manifests;artifactname=manifests;]${manifest}"
+    # echo "##vso[artifact.associate type=container;artifactname=manifests]#${manifest}"
 done
 
