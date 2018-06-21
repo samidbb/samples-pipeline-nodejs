7c20a58e91c0dbe4a8989b869090e4669a219295
Jens Andresen <jandr@DFDS.COM>
2018-06-21 15:01:52 +0200

    add fix for failing unit test

--- src/test/test.js
+++ src/test/test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 
 describe("lala", () => {
   it("can run a simple unit test", () => {
-    // assert.equal(true, true);
-    assert.fail();
+    assert.equal(true, true);
+    // assert.fail();
   });
 });
